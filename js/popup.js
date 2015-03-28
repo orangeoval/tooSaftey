7c915a9fc0c49d7b0e9e51da937679104c088d84
orangeoval <arderyp@gmail.com>
2015-03-27 23:49:27 -0400

    removed debug break

--- js/popup.js
+++ js/popup.js
@@ -66,11 +66,9 @@ $(document).ready(function() {
                     excludebutton.html('<i>[host excluded]</i>');
                     can_exclude = false;
     
-                    // Redirect back to http
-                    //alert("BLAH: " + current.http_url);
+                    // Redirect back to http version of site
                     command = 'window.location.replace("' + current.http_url + '")';
                     chrome.tabs.executeScript(current.id,{code: command}, function(result) {});
-
                     return false;
                 }
 
